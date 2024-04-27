068e14619ee04dcbd1145c39e2a20d913ea4188c
tomasitobambito <tomgacek0604@gmail.com>
2024-04-27 19:15:47 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft Modding Tutorial
+
+This repository houses all code related to [this](https://www.youtube.com/playlist?list=PLKGarocXCE1EO43Dlf5JGh7Yk-kRAXUEJ) series of tutorials.
---
+++ src/main/java/net/tomasitobambito/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.tomasitobambito.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
