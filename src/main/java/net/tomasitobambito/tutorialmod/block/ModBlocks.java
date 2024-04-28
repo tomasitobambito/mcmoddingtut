711f4d5d63a6d37a677929d73a8aa3b50862e195
tomasitobambito <tomgacek0604@gmail.com>
2024-04-28 14:52:12 +0200

    Added ruby ore blocks and corresponding loot tables / tags

--- src/main/java/net/tomasitobambito/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/tomasitobambito/tutorialmod/block/ModBlocks.java
@@ -1,15 +1,13 @@
 package net.tomasitobambito.tutorialmod.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.MapColor;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.tomasitobambito.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -18,10 +16,22 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.copy(Blocks.DIAMOND_BLOCK)
                     .mapColor(MapColor.RED)
                     .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(AbstractBlock.Settings.copy(Blocks.DIAMOND_BLOCK)
                     .mapColor(MapColor.DARK_CRIMSON)));
+
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.copy(Blocks.STONE).strength(2f)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.copy(Blocks.DEEPSLATE).strength(4f)));
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.copy(Blocks.NETHERRACK).strength(1.5f)));
+    public static final Block ENDSTONE_RUBY_ORE = registerBlock("endstone_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.copy(Blocks.END_STONE).strength(2f)));
 
 
     private static Block registerBlock(String name, Block block) {
--- src/main/java/net/tomasitobambito/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/tomasitobambito/tutorialmod/item/ModItemGroups.java
@@ -20,6 +20,11 @@ public class ModItemGroups {
 
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
+
+                        entries.add(ModBlocks.RUBY_ORE);
+                        entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
+                        entries.add(ModBlocks.NETHER_RUBY_ORE);
+                        entries.add(ModBlocks.ENDSTONE_RUBY_ORE);
                     }).build());
 
     public static void registerItemGroups() {
